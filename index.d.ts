fc8eb097b5724f83d3cca9e6809ae997a3e0dbb0
Socheat Sok <alex@socheat.net>
2021-08-16 11:00:50 +0700

    feat: build v1.0.0

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,10 @@
 declare type Maybe = PromiseLike<unknown>;
 declare type Yes = (value: unknown) => void;
 declare type No = (reason?: any) => void;
-export declare function areYouReadyYet(): {
-    maybe: Maybe;
+interface AreYouReadyYet {
+    maybe: () => Maybe;
     yes: Yes;
     no: No;
-};
+}
+export declare function areYouReadyYet(): AreYouReadyYet;
 export {};
